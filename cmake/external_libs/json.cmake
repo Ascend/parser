d15a1832612957c6900929677c0e428585ffb34a
taoxiangdong <taoxiangdong1@huawei.com>
2020-09-30 12:15:01 +0800

    update cmake

---
+++ cmake/external_libs/json.cmake
@@ -0,0 +1,23 @@
+if (HAVE_JSON)
+    return()
+endif()
+
+include(ExternalProject)
+
+set(JSON_SRC_DIR ${PARSER_DIR}/../third_party/json/include)
+ExternalProject_Add(json_build
+                    #URL https://github.com/nlohmann/json/releases/download/v3.6.1/include.zip
+                    #URL /home/txd/workspace/cloud_code/pkg/include.zip
+                    SOURCE_DIR  ${JSON_SRC_DIR}
+                    CONFIGURE_COMMAND ""
+                    BUILD_COMMAND ""
+                    INSTALL_COMMAND ""
+                    EXCLUDE_FROM_ALL TRUE 
+)
+
+
+add_library(json INTERFACE)
+target_include_directories(json INTERFACE ${JSON_SRC_DIR})
+add_dependencies(json json_build)
+
+set(HAVE_JSON TRUE CACHE BOOL "json build add")
