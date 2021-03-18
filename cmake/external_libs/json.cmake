4d72c0cebe1c2e68e7d9e418858071e02ea89b9c
wqtshg <wangtao123@huawei.com>
2021-03-18 16:42:50 +0800

    update submodule

--- cmake/external_libs/json.cmake
+++ cmake/external_libs/json.cmake
@@ -5,15 +5,19 @@ endif()
 include(ExternalProject)
 
 set(JSON_SRC_DIR ${CMAKE_BINARY_DIR}/opensrc/json/include)
+if (PARSER_PB_PKG)
+    set(REQ_URL "${PARSER_PB_PKG}/libs/ge_nlohmann_json/include.zip")
+    set(MD5 "0dc903888211db3a0f170304cd9f3a89")
+    set(JSON_INCLUDE_DIR ${JSON_SRC_DIR})
 #if (ENABLE_GITEE)
 #    set(REQ_URL "https://gitee.com/mirrors/JSON-for-Modern-CPP/repository/archive/v3.6.1.zip")
 #    set(MD5 "5bda78ce308e6cfcf614dcf1d5ff27a7")
 #    set(JSON_INCLUDE_DIR "${JSON_SRC_DIR}/include")
-#else()
+else()
     set(REQ_URL "https://github.com/nlohmann/json/releases/download/v3.6.1/include.zip")
     set(MD5 "0dc903888211db3a0f170304cd9f3a89")
     set(JSON_INCLUDE_DIR ${JSON_SRC_DIR})
-#endif ()
+endif ()
 ExternalProject_Add(json_build
                     URL ${REQ_URL}
                     #URL /home/txd/workspace/cloud_code/pkg/include.zip
