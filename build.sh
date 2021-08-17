d5f4b6da74094526fcc78c07ff73d1e714ad2af9
likun104 <likun104@huawei.com>
2021-08-17 18:20:17 +0800

    add new package for compiler

--- build.sh
+++ build.sh
@@ -255,14 +255,14 @@ generate_package_for_cann()
   cd "${BASEPATH}"
 
   PARSER_LIB_PATH="lib"
-  CMPL_PATH="compiler/lib64"
+  COMPILER_PATH="compiler/lib64"
 
   COMMON_LIB=("libgraph.so" "libregister.so" "liberror_manager.so")
   PARSER_LIB=("lib_caffe_parser.so" "libfmk_onnx_parser.so" "libfmk_parser.so" "libparser_common.so")
 
-  rm -rf ${OUTPUT_PATH:?}/${CMPL_PATH}/
+  rm -rf ${OUTPUT_PATH:?}/${COMPILER_PATH}/
 
-  mk_dir "${OUTPUT_PATH}/${CMPL_PATH}"
+  mk_dir "${OUTPUT_PATH}/${COMPILER_PATH}"
 
   find output/ -name parser_lib.tar -exec rm {} \;
 
@@ -270,15 +270,15 @@ generate_package_for_cann()
 
   for lib in "${PARSER_LIB[@]}";
   do
-    find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "$lib" -exec cp -f {} ${OUTPUT_PATH}/${CMPL_PATH} \;
+    find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "$lib" -exec cp -f {} ${OUTPUT_PATH}/${COMPILER_PATH} \;
   done
 
   for lib in "${COMMON_LIB[@]}";
   do
-    find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "$lib" -exec cp -f {} ${OUTPUT_PATH}/${CMPL_PATH} \;
+    find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "$lib" -exec cp -f {} ${OUTPUT_PATH}/${COMPILER_PATH} \;
   done
 
-  find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "libc_sec.so" -exec cp -f {} ${OUTPUT_PATH}/${CMPL_PATH} \;
+  find ${OUTPUT_PATH}/${PARSER_LIB_PATH} -maxdepth 1 -name "libc_sec.so" -exec cp -f {} ${OUTPUT_PATH}/${COMPILER_PATH} \;
 
   tar -cf parser_lib.tar compiler
 }
