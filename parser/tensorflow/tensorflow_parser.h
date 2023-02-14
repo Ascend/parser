107a911d7025fbc481fcf0831b177f79983ca2aa
guopeian <guopeian1@hisilicon.com>
2023-02-14 09:57:27 +0000

    !780 tf场景下GraphDef支持根图拆分const算子weight Merge pull request !780 from guopeian/graphdef_separate

--- parser/tensorflow/tensorflow_parser.h
+++ parser/tensorflow/tensorflow_parser.h
@@ -156,6 +156,34 @@ class PARSER_FUNC_VISIBILITY TensorFlowModelParser : public domi::ModelParser {
   Status ParseProtoWithSubgraph(const std::string &root_proto, domi::GetGraphCallbackV2 callback,
                                 ge::ComputeGraphPtr &root_graph) override;
 
+  /**
+   * @ingroup domi_omg
+   * @brief Analyze callback model data in subgraph
+   * @param [in] partitioned_serialized partitioned serialized network model
+   * @param [in] const_value_map const value map, key: constant node name value: serialized constant output tensor
+   * @param [in] callback callback of subgraph
+   * @param [in|out] graph Save the network information after analysis
+   * @return SUCCESS
+   * @return Others failed
+   */
+  Status ParseProtoWithSubgraph(const std::vector<std::string> &partitioned_serialized,
+                                const std::map<std::string, std::string> &const_value_map,
+                                domi::GetGraphCallbackV2 callback,
+                                ge::ComputeGraphPtr &root_graph) override;
+
+  /**
+   * @ingroup domi_omg
+   * @brief Analyze network model data
+   * @param [in] partitioned_serialized partitioned serialized network model
+   * @param [in] const_value_map const value map, key: constant node name value: serialized constant output tensor
+   * @param [in|out]  graph Save the network information after analysis
+   * @return SUCCESS
+   * @return Others failed
+   */
+  Status ParseProto(const std::vector<std::string> &partitioned_serialized,
+                    const std::map<std::string, std::string> &const_value_map,
+                    ge::ComputeGraphPtr &graph) override;
+
   bool HasError() override {
     return PreChecker::Instance().HasError();
   }
