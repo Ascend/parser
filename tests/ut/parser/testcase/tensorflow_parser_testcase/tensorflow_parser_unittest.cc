c7ec5aeec166cba8a1566a0027885e4d812f7a0c
gengchao <gengchao4@huawei.com>
2023-08-03 13:45:27 +0000

    !868 更新submodel Merge pull request !868 from gengchao/submodel_update

--- tests/ut/parser/testcase/tensorflow_parser_testcase/tensorflow_parser_unittest.cc
+++ tests/ut/parser/testcase/tensorflow_parser_testcase/tensorflow_parser_unittest.cc
@@ -3716,28 +3716,6 @@ TEST_F(UtestTensorflowParser, tensorflow_Pb2Json_RepeatedMessage2Json_test)
   delete field;
 }
 
-TEST_F(UtestTensorflowParser, tensorflow_Pb2Json_OneField2Json_test)
-{
-  Pb2Json toJson;
-  tensorflow::NodeDef *node_def = initNodeDef();
-  ProtobufFieldDescriptor *field = new google::protobuf::FieldDescriptor();
-  ProtobufReflection *reflection = nullptr;
-  set<string> black_fields;
-  black_fields.emplace("offset");
-  Json json;
-  bool enum2str = true;
-
-  Message2Operator mess2Op;
-  int depth = 4;
-  ge::OpDescPtr op_desc = std::make_shared<ge::OpDesc>("FusionCustom", "FusionCustom");
-  ge::Operator ops = ge::OpDescUtils::CreateOperatorFromOpDesc(op_desc);
-  field->CppTypeName(google::protobuf::FieldDescriptor::CPPTYPE_ENUM);
-  mess2Op.ParseField(reflection, node_def, field, depth, ops);
-  toJson.OneField2Json((*node_def), field, reflection, black_fields, json, enum2str, 1);
-  toJson.OneField2Json((*node_def), field, reflection, black_fields, json, enum2str, 5);
-  delete field;
-}
-
 TEST_F(UtestTensorflowParser, input_proto_real_path_success) {
   const char *caffe_proto_path = "./caffe/caffe.proto";
   const char *custom_proto_path = "./caffe/custom.proto";
